bb6d0fb1e67090bd5358575ff7f6c1a4648c16be
CaldRicardo <rickycalderon2006@gmail.com>
2025-02-24 10:55:43 -0400

    Aqui ta el archivo JOSE

---
+++ FindReduce.js
@@ -0,0 +1,8 @@
+const personas = [
+    {nombre}
+
+    const numeros = [1 ,2 ,3 ,4 ,5 ];
+    const Sumatotal = numeros. reduce((acumulador , numero) => acumulador + numero , 0);
+    console.log(Sumatotal);
+
+]
